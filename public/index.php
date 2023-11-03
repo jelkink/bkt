8635b7a578750f5f8d54029bc084b0f21d7e0176
Jos Elkink <jelkink@gmail.com>
2023-11-03 18:16:16 +0000

    Separate files for public directory from hidden folder.

--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php
 
-require_once("../src/classes/class.database.inc.php");
-require_once("../src/classes/class.httpcontext.inc.php");
-require_once("../src/classes/class.session.inc.php");
-require_once("../src/classes/class.gui.inc.php");
+$path_to_library = "../src/";
+
+require_once($path_to_library . "classes/class.database.inc.php");
+require_once($path_to_library . "classes/class.httpcontext.inc.php");
+require_once($path_to_library . "classes/class.session.inc.php");
+require_once($path_to_library . "classes/class.gui.inc.php");
 
 $dbh = new Database;
 $gui = new GUI;
@@ -27,7 +29,7 @@ if (!$dbh->open()) {
 		$gui->AddCenter($session->LoginForm());
 	} else if ($page == "login") {
 
-		include("page.login.inc.php");
+		include($path_to_library . "page.login.inc.php");
 	} else {
 
 		if ($session->VerifyLogin(HTTPContext::getString('session', ''))) {
@@ -38,15 +40,16 @@ if (!$dbh->open()) {
 			"<a href=\"index.php?page=list_accounts&session=$session->id\">List accounts</a><br />\n" .
 			"<a href=\"index.php?page=list_bookings&session=$session->id\">List bookings</a><br />\n" .
 			"<a href=\"index.php?page=list_notes&session=$session->id\">List notes</a><br />\n" .
+			"<a href=\"index.php?page=list_tags&session=$session->id\">List tags</a><br />\n" .
 			"<a href=\"index.php?page=list_templates&session=$session->id\">List templates</a><br />\n" .
 			"<a href=\"index.php?page=list_currencies&session=$session->id\">List currencies</a><br />\n" .
 			"<a href=\"index.php?page=export&session=$session->id\">Export to BKT</a><br />\n" .
 			"<a href=\"index.php?page=export_csv&session=$session->id\" target=\"_blank\">Export to CSV</a><br />\n" .
 			"<br /><br /><a href=\"index.php?page=logout&session=$session->id\">Logout</a><br />\n");
 
-			if (file_exists("../src/page." . $page . ".inc.php")) {
+			if (file_exists($path_to_library . "page." . $page . ".inc.php")) {
 
-				include("../src/page." . $page . ".inc.php");
+				include($path_to_library . "page." . $page . ".inc.php");
 			} else {
 
 				$msg = "ERROR: Page " . $page . " does not exist.";
