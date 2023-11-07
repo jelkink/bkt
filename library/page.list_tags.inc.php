6a29b57ae3b8c5d6b69d9f31a8a226e6b9304837
Jos Elkink <jelkink@gmail.com>
2023-11-07 09:03:06 +0000

    Add listing tags

--- library/page.list_tags.inc.php
+++ library/page.list_tags.inc.php
@@ -1,14 +1,15 @@
 <?php
 
-$ref = do_query("SELECT tag FROM tags GROUP BY tag");
+$ref = do_query("SELECT tag, COUNT(tag) AS cnt FROM tags GROUP BY tag");
 
 $gui->AddCenter("<center><table class=\"standardTable\">\n");
-$gui->AddCenter("<tr><th>tag</th><th></th></tr>\n");
+$gui->AddCenter("<tr><th>tag</th><th>count</th><th></th></tr>\n");
 
 while ($tag = mysqli_fetch_object($ref)) {
 
-  $gui->AddCenter("<tr><td style=\"text-align: right\">" .
+  $gui->AddCenter("<tr>" .
     "<td style=\"text-align: left\"><a href=\"index.php?page=list_bookings&session=$session->id&tag=$tag->tag\">$tag->tag</a></td>" .
+    "<td>$tag->cnt</td>" .
     "<td><a href=\"index.php?page=delete_tag&session=$session->id&tag=$tag->tag\"><img src=\"images/bin.png\" width=\"20px\"></a></td>" .
     "</tr>\n");
 }
